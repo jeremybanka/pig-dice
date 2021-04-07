421869d77d497ee9732d5929204fb376cb7d3076
basicjohn <edmondsonj@gmail.com>
2021-04-07 15:50:28 -0700

    add player.hold functionality
    Co-authored-by: Jeremy Banka <hello@jeremybanka.com>

--- js/core.js
+++ js/core.js
@@ -1,7 +1,20 @@
+function Game() {
+  this.gameHasStarted = false;
+  this.gamesPlayed = 0;
+  this.players = [];
+}
+
+Game.prototype.addPlayer = function(name) {
+  const player = new Player(name);
+  this.players.push(player);
+}
+
+
+
 function Player(name) {
   this.name = name;
   this.score = {
-    current: 0,
+    turn: 0,
     total: 0,
   }
   this.gamesWon = 0;
@@ -10,15 +23,21 @@ function Player(name) {
 Player.prototype.roll = function() {
   const randomRoll = Math.floor(Math.random() * 6) + 1
   if (randomRoll === 1) {
-    this.score.current = 0
+    this.score.turn = 0
     return 0
   }
-  this.score.current += randomRoll
+  this.score.turn += randomRoll
   return randomRoll
 }
 
+Player.prototype.hold = function() {
+  this.score.total += this.score.turn;
+  this.score.turn = 0;
+}
 
 
-const name = "steve";
-const player1 = new Player(name);
-
+const game = new Game();
+game.addPlayer("Steve")
+game.addPlayer("Mary")
+const steve = game.players[0]
+const mary = game.players[1]
