b351452911c67fc580941fda9e6aa5e3bc875b6e
Isaac Moreno <ipdxcreative@gmail.com>
2021-04-13 16:35:53 -0700

    🚧 add an unfinished test
    Co-authored-by: Jeremy Banka <hello@jeremybanka.com>

---
+++ __test__/Player.test.js
@@ -0,0 +1,12 @@
+import Player from "../src/js/Player"
+
+// export default function Player(name) {
+//   this.name = name
+//   this.score = { turn: 0, total: 0 }
+//   this.gamesWon = 0
+// }
+
+  describe(``, () => {
+    
+  })
+  
